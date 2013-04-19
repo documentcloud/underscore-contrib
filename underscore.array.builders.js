1a1fabeef22f6712523b7533f626a2e39b23d167
fogus <mefogus@gmail.com>
2013-04-19 07:59:37 -0400

    Starting on small subset of contrib library to ensure that all of the pieces are in order (test infrastructure, docs, mixin decls, etc.  Speed and such will come iteratively.

---
+++ underscore.array.builders.js
@@ -0,0 +1,39 @@
+//     Underscore-contrib
+//     underscore.array.builders.js 0.0.1
+//     http://underscorejs.org
+//     (c) 2013 Michael Fogus
+//     Underscore-contrib may be freely distributed under the MIT license.
+
+(function(root) {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  var _ = root._ || require('underscore');
+
+  // Helpers
+  // -------
+  
+  var slice   = ArrayProto.slice,
+      concat  = ArrayProto.concat;
+
+
+  // Mixing in the array builders
+  // ----------------------------
+
+  _.mixin({
+    // Concatenates one or more arrays given as arguments
+    cat: function() {
+      var head = _.first(arguments);
+      var args = _.rest(arguments);
+      if (head != null) {
+        return concat.apply(head, args);
+      }
+      else {
+        return [];
+      }
+    }
+  });
+
+})(this);
