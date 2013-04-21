cf2af8995f5ada6a2bbf6e359f70f0aaf576e0db
fogus <mefogus@gmail.com>
2013-04-21 10:23:25 -0400

    Template for function.combinators

---
+++ underscore.function.combinators.js
@@ -0,0 +1,23 @@
+// Underscore-contrib (underscore.function.combinators.js 0.0.1)
+// (c) 2013 Michael Fogus and DocumentCloud Inc.
+// Underscore-contrib may be freely distributed under the MIT license.
+
+(function(root) {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  var _ = root._ || require('underscore');
+
+  // Helpers
+  // -------
+  
+  // Mixing in the array builders
+  // ----------------------------
+
+  _.mixin({
+
+  });
+
+})(this);
