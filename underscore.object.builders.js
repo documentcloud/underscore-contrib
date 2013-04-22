86915e6562ba1cbef414e251aa7fdc3bc67f2c36
fogus <mefogus@gmail.com>
2013-04-22 13:48:24 -0400

    Added merge object builder

--- underscore.object.builders.js
+++ underscore.object.builders.js
@@ -12,12 +12,30 @@
 
   // Helpers
   // -------
+
+  // Create quick reference variables for speed access to core prototypes.
+  var slice   = Array.prototype.slice,
+      concat  = Array.prototype.concat;
+
+  var existy = function(x) { return x != null; };
+  var truthy = function(x) { return (x !== false) && existy(x); };
   
   // Mixing in the object builders
   // ----------------------------
 
   _.mixin({
+    // Merges two or more objects starting with the left-most and
+    // applying the keys right-word
+    // {any:any}* -> {any:any}
+    merge: function(/* objs */){
+      var dest = _.some(arguments) ? {} : null;
 
+      if (truthy(dest)) {
+        _.extend.apply(null, concat.call([dest], _.toArray(arguments)));
+      }
+
+      return dest;
+    }
   });
 
 })(this);
