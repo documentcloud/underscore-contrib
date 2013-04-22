48e7999ab3e11c88c50d517aa3b749ce937bf552
fogus <mefogus@gmail.com>
2013-04-22 13:52:32 -0400

    Added isIncreasing and isDecreasing predciates

--- underscore.function.predicates.js
+++ underscore.function.predicates.js
@@ -24,9 +24,41 @@
     isSequential: function(x) { return (_.isArray(x)) || (_.isArguments(x)); },
     isZero: function(x) { return 0 === x; },
     isEven: function(x) { return (x & 1) === 0; },
-    isOdd: function(x) { return !L.isEven(x); },
+    isOdd: function(x) { return !_.isEven(x); },
     isPositive: function(x) { return x > 0; },
-    isNegative: function(x) { return x < 0; }
+    isNegative: function(x) { return x < 0; },
+    
+    // Returns true if its arguments are monotonically
+    // increaing values; false otherwise.
+    isIncreasing: function() {
+      var count = _.size(arguments);
+      if (count === 1) return true;
+      if (count === 2) return arguments[0] < arguments[1];
+
+      for (var i = 1; i < count; i++) {
+        if (arguments[i-1] >= arguments[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    },
+
+    // Returns true if its arguments are monotonically
+    // decreaing values; false otherwise.
+    isDecreasing: function() {
+      var count = _.size(arguments);
+      if (count === 1) return true;
+      if (count === 2) return arguments[0] > arguments[1];
+
+      for (var i = 1; i < count; i++) {
+        if (arguments[i-1] <= arguments[i]) {
+          return false;
+        }
+      }
+
+      return true;
+    }
   });
 
 })(this);
