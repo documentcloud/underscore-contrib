f04253c203315949a8f136062ad8f6d2dbac27a5
fogus <mefogus@gmail.com>
2013-04-22 13:37:52 -0400

    Placeholder for object builders

---
+++ underscore.object.builders.js
@@ -0,0 +1,23 @@
+// Underscore-contrib (underscore.object.builders.js 0.0.1)
+// (c) 2013 Michael Fogus and DocumentCloud Inc.
+// Underscore-contrib may be freely distributed under the MIT license.
+
+(function(root) {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  var _ = root._ || require('underscore');
+
+  // Helpers
+  // -------
+  
+  // Mixing in the object builders
+  // ----------------------------
+
+  _.mixin({
+
+  });
+
+})(this);
