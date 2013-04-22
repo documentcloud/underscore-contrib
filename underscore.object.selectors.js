08b779fc92f56339671a91de62a426f9108b630c
fogus <mefogus@gmail.com>
2013-04-22 14:39:20 -0400

    Placeholder for object selectors

---
+++ underscore.object.selectors.js
@@ -0,0 +1,31 @@
+// Underscore-contrib (underscore.object.selectors.js 0.0.1)
+// (c) 2013 Michael Fogus and DocumentCloud Inc.
+// Underscore-contrib may be freely distributed under the MIT license.
+
+(function(root) {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  var _ = root._ || require('underscore');
+
+  // Helpers
+  // -------
+
+  // Create quick reference variables for speed access to core prototypes.
+  var slice   = Array.prototype.slice,
+      concat  = Array.prototype.concat;
+
+  var existy = function(x) { return x != null; };
+  var truthy = function(x) { return (x !== false) && existy(x); };
+  var isAssociative = function(x) { return _.isArray(x) || _.isObject(x); };
+  
+  // Mixing in the object selectors
+  // ------------------------------
+
+  _.mixin({
+
+  });
+
+})(this);
