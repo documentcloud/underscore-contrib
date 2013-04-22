13012e9331724aabd3ebceda6a571fc71563b691
fogus <mefogus@gmail.com>
2013-04-22 15:07:40 -0400

    Initial version of a trampoline.  Somewhere between a Bilby.js version and a Lemonad version.

--- underscore.util.trampolines.js
+++ underscore.util.trampolines.js
@@ -18,7 +18,22 @@
   // ------------------------
 
   _.mixin({
+    done: function(value) {
+      var ret = _(value);
+      ret.stopTrampoline = true;
+      return ret;
+    },
 
+    trampoline: function(fun /*, args */) {
+      var result = fun.apply(fun, _.rest(arguments));
+
+      while (_.isFunction(result)) {
+        result = result();
+        if ((result instanceof _) && (result.stopTrampoline)) break;
+      }
+
+      return result.value();
+    }
   });
 
 })(this);
