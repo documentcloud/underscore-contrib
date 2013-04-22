1af3e9cc713a970bd4221c30820acf86e6c37dd0
fogus <mefogus@gmail.com>
2013-04-22 13:11:00 -0400

    Added fix function

--- underscore.function.arity.js
+++ underscore.function.arity.js
@@ -18,7 +18,29 @@
   // -----------------------------
 
   _.mixin({
+    // ### Fixed arguments
 
+    // Fixes the arguments to a function based on the parameter template defined by
+    // presence of values and the `_` placeholder.
+    fix: function(fun) {
+      var args = _.rest(arguments);
+
+      var f = function() {
+        var arg = 0;
+
+        for ( var i = 0; i < args.length && arg < arguments.length; i++ ) {
+          if ( args[i] === _ ) {
+            args[i] = arguments[arg++];
+          }
+        }
+
+        return fun.apply(null, args);
+      };
+
+      f._original = fun;
+
+      return f;
+    }
   });
 
 })(this);
