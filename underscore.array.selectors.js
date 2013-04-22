105a33b0b5579de9d3ec77550a687a1cbcaac22b
fogus <mefogus@gmail.com>
2013-04-22 09:51:00 -0400

    Placeholder for array selectors library

---
+++ underscore.array.selectors.js
@@ -0,0 +1,28 @@
+// Underscore-contrib (underscore.array.selectors.js 0.0.1)
+// (c) 2013 Michael Fogus and DocumentCloud Inc.
+// Underscore-contrib may be freely distributed under the MIT license.
+
+(function(root) {
+
+  // Baseline setup
+  // --------------
+
+  // Establish the root object, `window` in the browser, or `global` on the server.
+  var _ = root._ || require('underscore');
+
+  // Helpers
+  // -------
+  
+  // Create quick reference variables for speed access to core prototypes.
+  var slice   = Array.prototype.slice,
+      concat  = Array.prototype.concat;
+
+
+  // Mixing in the array selectors
+  // ----------------------------
+
+  _.mixin({
+
+  });
+
+})(this);
