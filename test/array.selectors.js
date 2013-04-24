11a0858dac0a43a80e8394a04c7c9fe15e211ac0
fogus <mefogus@gmail.com>
2013-04-24 08:49:26 -0400

    Added take/dropWhile tests

--- test/array.selectors.js
+++ test/array.selectors.js
@@ -7,4 +7,22 @@ $(document).ready(function() {
 
     equal(_.second(a), 2, 'should retrieve the 2nd element in an array');
   });
+
+  test("takeWhile", function() {
+    var isNeg = function(n) { return n < 0; };
+
+    deepEqual(_.takeWhile([-2,-1,0,1,2], isNeg), [-2,-1], 'should take elements until a function goes truthy');
+    deepEqual(_.takeWhile([1,-2,-1,0,1,2], isNeg), [], 'should take elements until a function goes truthy');
+  });
+
+  test("dropWhile", function() {
+    var isNeg = function(n) { return n < 0; };
+
+    deepEqual(_.dropWhile([-2,-1,0,1,2], isNeg), [0,1,2], 'should drop elements until a function goes truthy');
+    deepEqual(_.dropWhile([0,1,2], isNeg), [0,1,2], 'should drop elements until a function goes truthy');
+    deepEqual(_.dropWhile([-2,-1], isNeg), [], 'should drop elements until a function goes truthy');
+    deepEqual(_.dropWhile([1,-2,-1,0,1,2], isNeg), [1,-2,-1,0,1,2], 'should take elements until a function goes truthy');
+    deepEqual(_.dropWhile([], isNeg), [], 'should handle empty arrays');
+  });
+
 });
