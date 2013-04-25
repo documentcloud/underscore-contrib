3d674eb46ae23315cbc5972c354a84d3a88ce093
fogus <mefogus@gmail.com>
2013-04-25 11:05:14 -0400

    Added tests for selectKeys

--- test/object.selectors.js
+++ test/object.selectors.js
@@ -5,9 +5,14 @@ $(document).ready(function() {
   test("accessor", function() {
     var a = [{a: 1, b: 2}, {c: 3}];
 
-    equal(_.accessor('a')(a[0]), 1, '');
-    equal(_.accessor('a')(a[1]), undefined, '');
-    deepEqual(_.map(a, _.accessor('a')), [1, undefined], '');
+    equal(_.accessor('a')(a[0]), 1, 'should return a function that plucks');
+    equal(_.accessor('a')(a[1]), undefined, 'should return a function that plucks, or returns undefined');
+    deepEqual(_.map(a, _.accessor('a')), [1, undefined], 'should return a function that plucks');
+  });
+
+  test("selectKeys", function() {
+    deepEqual(_.selectKeys({'a': 1, 'b': 2}, ['a']), {'a': 1}, 'shold return a map of the desired keys');
+    deepEqual(_.selectKeys({'a': 1, 'b': 2}, ['z']), {}, 'shold return an empty map if the desired keys are not present');
   });
 
 });
