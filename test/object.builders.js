1b7a8abc65ebe3d00fc4becaef263c39ccc0a9c9
fogus <mefogus@gmail.com>
2013-04-25 10:19:34 -0400

    Added tests for merge

--- test/object.builders.js
+++ test/object.builders.js
@@ -2,6 +2,17 @@ $(document).ready(function() {
 
   module("underscore.object.builders");
 
-  test("", function() {
+  test("merge", function() {
+    var o = {'a': 1, 'b': 2};
+
+    deepEqual(_.merge(o), {a: 1, b: 2}, 'should return a copy of the object if given only one');
+    deepEqual(_.merge({'a': 1, 'b': 2}, {b: 42}), {'a': 1, b: 42}, 'should merge two objects');
+    deepEqual(_.merge({a: 1, b: 2}, {b: 42}, {c: 3}), {a: 1, b: 42, c: 3}, 'should merge three or more objects');
+    deepEqual(_.merge({a: 1, b: 2}, {b: 42}, {c: 3}, {c: 4}), {a: 1, b: 42, c: 4}, 'should merge three or more objects');
+
+    var a = {'a': 1, 'b': 2};
+    var $ = _.merge(a, {'a': 42});
+
+    deepEqual(a, {'a': 1, 'b': 2}, 'should not modify the original');
   });
 });
