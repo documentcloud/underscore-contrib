c0f3525ae8eabfd45a6d119b6819476bbd988bfb
eborden <evan@evan-borden.com>
2013-05-28 13:57:22 -0400

    added curry method from https://github.com/eborden/js-currya

--- underscore.function.arity.js
+++ underscore.function.arity.js
@@ -88,10 +88,36 @@
         }
         else return fun(optionalFirst, last);
       };
-    }
+    },
     
+    // Flexible curry function with strict arity.
+    // source: https://github.com/eborden/js-curry
+    curry: (function () {
+      function checkArguments (argsCount) {
+        if (argsCount !== 1) {
+          throw new RangeError('Only a single argument may be accepted.');
+        }
+      }
+      function collectArgs(func, that, argCount, args, newArg) {
+        args.push(newArg);
+        if (args.length == argCount) {
+          return func.apply(that, args);
+        } else {
+          return function () {
+            checkArguments(arguments.length);
+            return collectArgs(func, that, argCount, args.slice(0), arguments[0]);
+          };
+        }
+      }
+      return function curry (func) {
+        var that = this;
+        return function () {
+          checkArguments(arguments.length);
+          return collectArgs(func, that, func.length, [], arguments[0]);
+        };
+      };
+    }())
   });
-  
   _.arity = (function () {
     var FUNCTIONS = {};
     return function arity (numberOfArgs, fun) {
