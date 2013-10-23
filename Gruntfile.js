701121b8db02df8f3869dd80f99776936e47c263
Joshua Clanton <joshua.clanton@gmail.com>
2013-10-23 18:54:00 -0400

    Add clarifying comment to ES3 option.

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,7 +43,7 @@ module.exports = function(grunt) {
         "test/*.js"
       ],
       options: {
-        es3: true,
+        es3: true,     // Enforce ES3 compatibility
         indent: 2,     // Indent by 2 spaces
         eqnull: true,  // Allow 'x == null' convention
         "-W058": false // Allow 'new Constructor' without parens
