7fce11aa5107770a2449a0a12e82abb143c13a5b
Joshua Clanton <joshua.clanton@gmail.com>
2013-11-20 18:03:36 -0500

    Noted todos in util.string tests.

--- test/util.strings.js
+++ test/util.strings.js
@@ -3,6 +3,11 @@ $(document).ready(function() {
 
   module('underscore.util.strings');
 
+  // TODO: Test implode
+  // TODO: Test explode
+  // TODO: Test camelCase
+  // TODO: Test toDash
+
   test('strContains', function() {
     equal(_.strContains('Metaphysics', 'physics'), true, 'Should return true if string contains search string.');
     equal(_.strContains('Poetics', 'prose'), false, 'Should return false if string does not contain search string.');
