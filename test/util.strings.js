4dab338c6d1381f4e6c2e06dc980e96216d5febc
Joshua Clanton <joshua.clanton@gmail.com>
2013-12-02 09:09:07 -0500

    Added unit tests for util.strings.

--- test/util.strings.js
+++ test/util.strings.js
@@ -3,10 +3,22 @@ $(document).ready(function() {
 
   module('underscore.util.strings');
 
-  // TODO: Test implode
-  // TODO: Test explode
-  // TODO: Test camelCase
-  // TODO: Test toDash
+  test('explode', function() {
+    deepEqual(_.explode('Virgil'), ['V','i','r','g','i','l'], 'Should explode a string into an array of characters.');
+  });
+
+  test('implode', function() {
+    equal(_.implode(['H','o','m','e','r']), 'Homer', 'Should implode an array of characters into a single string.');
+  });
+
+  test('camelCase', function() {
+    equal(_.camelCase('punic-wars'), 'punicWars', 'Should convert a dashed-format string to camelCase.');
+  });
+
+  test('toDash', function() {
+    equal(_.toDash('trojanWar'), 'trojan-war', 'Should convert a camelCase string to dashed-format.');
+    equal(_.toDash('PersianWar'), 'persian-war', 'Should convert a PascalCase string to dashed-format.');
+  });
 
   test('strContains', function() {
     equal(_.strContains('Metaphysics', 'physics'), true, 'Should return true if string contains search string.');
