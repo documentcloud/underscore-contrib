82a9165c20808ad9a0c98eba045ed35f31f6ef67
Jonah Dahlquist <jonah@nucleussystems.com>
2014-03-07 16:32:33 -0800

    Improved test coverage of toQuery and fromQuery functions

--- test/util.strings.js
+++ test/util.strings.js
@@ -8,10 +8,8 @@ $(document).ready(function() {
   });
 
   test('fromQuery', function() {
-    var query = 'foo%26bar=baz&test=success';
-    console.log(_.fromQuery(query));
-    console.log({'foo&bar': 'baz', 'test': 'success'});
-    ok(_.isEqual(_.fromQuery(query), {'foo&bar': 'baz', 'test': 'success'}), 'can convert a query string to a hash');
+    var query = 'foo%26bar=baz&test=total+success';
+    ok(_.isEqual(_.fromQuery(query), {'foo&bar': 'baz', 'test': 'total success'}), 'can convert a query string to a hash');
   });
 
   test('implode', function() {
@@ -28,8 +26,8 @@ $(document).ready(function() {
   });
 
   test('toQuery', function() {
-    var obj = {'foo&bar': 'baz', 'test': 'success'};
-    equal(_.toQuery(obj), 'foo%26bar=baz&test=success', 'can convert a hash to a query string');
+    var obj = {'foo&bar': 'baz', 'test': 'total success'};
+    equal(_.toQuery(obj), 'foo%26bar=baz&test=total%20success', 'can convert a hash to a query string');
   });
 
   test('strContains', function() {
